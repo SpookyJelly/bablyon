f90e31026b0cbda8a1efd63656c15540f98f0697
SpookyJelly <zang9412@gmail.com>
2023-08-18 15:15:04 +0900

    Updates

--- src/MainScene/stage/index.ts
+++
@@ -1,8 +0,0 @@
-import { aroundWall } from "./aroundWall";
-import { ground } from "./ground";
-import { houses } from "./houses";
-import { loadMobs } from "./mobs";
-import { obstacle } from "./obstacle";
-import { skybox } from "./skybox";
-
-export { aroundWall, ground, houses, loadMobs, obstacle, skybox };
--- src/SceneNo.ts
+++
@@ -1,3 +0,0 @@
-export enum SceneNo {
-  MainScene = 0,
-}
